43978ce89a510cd4e7f6b08f4a2003fde437427c
Sean Pollock <seanpollock3344@gmail.com>
2019-06-27 02:59:38 -0400

    updating compile function to include cwrap configuration

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-emcc contracts/Token.cpp -O0 -s EXPORTED_FUNCTIONS='["_init_contract"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -std=c++11
+emcc contracts/Token.cpp -O0 -s EXPORTED_FUNCTIONS='["_mint"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -std=c++11
 
 
 
