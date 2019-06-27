84bd3ca6cdc05430d85744ca2970df199b43fb31
Sean Pollock <seanpollock3344@gmail.com>
2019-06-26 20:26:54 -0400

    adding extern C interface

--- Token.cpp
+++
@@ -1,9 +0,0 @@
-#include "Token.h"
-#include "map"
-
-bool Token::transfer(char* recipient, unsigned int value) {
-  balances.insert(std::pair<char*, unsigned int>(recipient, value));
-  return true;
-}
-
-
--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-emcc Token.cpp -s WASM=1 -o hello.html
+emcc contracts/Token.cpp -O0 -s EXPORTED_FUNCTIONS='["_init_contract"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -std=c++11
+
+
 
