87924d47396d52d9aca0b7a3bdd0a457f2027d33
Sean Pollock <seanpollock3344@gmail.com>
2019-07-01 17:05:22 -0400

    transfer function

--- test.js
+++ test.js
@@ -2,14 +2,7 @@ const contractModule = require("./a.out.js")
 
 contractModule.onRuntimeInitialized = () => {
   console.log("setting owner of contract")
-  const owner = "sean"
-  const ownerBuffer = contractModule._malloc(owner.length + 1)
-  console.log("ownerBuffer.size", typeof ownerBuffer)
-  console.log("ownerBuffer", ownerBuffer)
-  contractModule.writeStringToMemory(owner, ownerBuffer) 
-  console.log("ownerBuffer", ownerBuffer)
   const setOwner = contractModule.cwrap("setOwner", "bool", ["string"])
-  console.log("ownerbuffer type", typeof [ownerBuffer])
   const setOwnerResult = setOwner("sean")
   console.log("setOwnerResult", ownerBuffer)
 
@@ -22,7 +15,6 @@ contractModule.onRuntimeInitialized = () => {
   const getSupply = contractModule.cwrap("getSupply", "number")
   const supply = getSupply()
   console.log("total supply is", supply)
-
 
   console.log("calling transfer method")
   const transfer = contractModule.cwrap("transfer", "bool", ["string", "number"])
