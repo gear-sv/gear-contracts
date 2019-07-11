6988f496640c4fabc9a3c5f0a833bdca7850f7b6
Sean Pollock <seanpollock3344@gmail.com>
2019-07-10 20:27:46 -0400

    fixing char* comparisons. adding constructor with hardcoded owner. 10 test cases

--- contracts/Token.cpp
+++ contracts/Token.cpp
@@ -1,26 +1,41 @@
 #include "Token.h"
 #include "map"
 
-
 /*
  *  sets bitcoin address owner of the contract
  */
-bool Token::setOwner(char* SENDER, char* _owner) {
-  if (SENDER != owner) {
-    return false;    
+bool Token::setOwner(char* SENDER, char* newOwner) {
+  // check if SENDER is the current owner
+  if (strcmp(SENDER, owner) != 0) {
+    return false;
   }
 
-  owner = _owner;
+  // set new owner
+  owner = newOwner;
+
   return true;
 }
 
 /*
- * mints amount of tokens, issues to owner
+ * mints value of tokens, issues to owner
  */
-bool Token::mint(char* SENDER, unsigned int amount) {
-  int balance = balances[sender];
-  std::cout <<  "balance of sender in contract is" << balance;
-  supply = supply + amount;
+bool Token::mint(char* SENDER, unsigned int value) {
+  // check if SENDER is the owner
+  if (strcmp(SENDER, owner) != 0) {
+    return false;
+  }
+
+  // increment supply
+  supply = supply + value;
+
+  // increment owner balance
+  if (balances.find(SENDER) == balances.end()) {
+    balances.insert(std::pair<char*, unsigned int>(SENDER, value));
+  } else {
+    int balance = balances[SENDER];
+    balances[SENDER] = balance + value;
+  }
+
   return true;
 }
 
@@ -28,7 +43,23 @@ bool Token::mint(char* SENDER, unsigned int amount) {
  * transfers value of tokens to a recipient
  */
 bool Token::transfer(char* SENDER, char* recipient, unsigned int value) {
-  balances.insert(std::pair<char*, unsigned int>(recipient, value));
+  // check if SENDER has sufficient funds
+  if (balances[SENDER] < value) {
+    return false;
+  }
+
+  // increment recipient balance
+  if (balances.find(recipient) == balances.end()) {
+    balances.insert(std::pair<char*, unsigned int>(recipient, value));
+  } else {
+    int recipientBalance = balances[recipient];
+    balances[recipient] = recipientBalance + value;
+  }
+
+  // decrement SENDER balance
+  int senderBalance = balances[SENDER];
+  balances[SENDER] = senderBalance - value;
+
   return true;
 }
 
@@ -41,18 +72,17 @@ Token token;
  * Interface
  */
 extern "C" {
+
   bool mint(char* sender, int amount) {
-    token.mint(sender, amount);   
-    return true;
-  }       
-  
+    return token.mint(sender, amount);
+  }
+
   int getSupply() {
     return token.supply;
   }
-  
+
   bool setOwner(char* sender, char* owner) {
-    token.setOwner(sender, owner);
-    return true;
+    return token.setOwner(sender, owner);
   }
 
   char* getOwner() {
@@ -60,8 +90,10 @@ extern "C" {
   }
 
   bool transfer(char* sender, char* recipient, int value) {
-    token.transfer(sender, recipient, value);
-    return true;
+    return token.transfer(sender, recipient, value);
   }
 
+  int getBalance(char* address) {
+    return token.balances[address];
+  }
 }
--- contracts/Token.h
+++ contracts/Token.h
@@ -1,5 +1,5 @@
-/*  
- * ERC20 interface as defined: 
+/*
+ * ERC20 interface as defined:
  * https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC20/ERC20.sol
  */
 #include <map>
@@ -9,17 +9,19 @@ class Token {
   public:
     // represents integer value for total number of outstanding tokens
     unsigned int supply;
-    
-    // current owner of the contract
-    char* owner; 
 
-    // current balance accounting  
+    // current owner of the contract
+    char* owner;
+
+    Token() {
+      owner = "13geTUt3kqJ3vxMMBbWZwJ1NXnSEK2VDm6";
+    }
+    // current balance accounting
     std::map<char*, unsigned int> balances;
-    
-    bool mint(char* SENDER, unsigned int amount);
+
+    bool mint(char* sender, unsigned int value);
 
     bool transfer(char* SENDER, char* recipient, unsigned int value);
-    
-    bool setOwner(char* SENDER, char* _owner);
-};
 
+    bool setOwner(char* SENDER, char* newOwner);
+};
