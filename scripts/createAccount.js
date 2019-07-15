c082b79662f781801c0f1d323c2fed0422b5950d
Sean Pollock <seanpollock3344@gmail.com>
2019-07-15 11:11:05 -0400

    create account script. privateKey, publicKey, address

--- scripts/createAccount.js
+++ scripts/createAccount.js
@@ -1,10 +1,21 @@
 const bsv = require("bsv")
 const fs = require("fs")
 
-const privateKey = new bsv.PrivateKey()
-const exported = privateKey.toWIF()
+const privateKey = bsv.PrivateKey.fromRandom()
+console.log("private Key", privateKey.toString())
 
+const publicKey = bsv.PublicKey.fromPrivateKey(privateKey)
+console.log("publicKey", publicKey)
 
-fs.writeFile("key.json", JSON.stringify({ privateKey: exported }), "utf8", () => {
+const address = bsv.Address.fromPublicKey(publicKey).toString()
+console.log("address", address)
+
+const key = {
+  privateKey: privateKey.toString(),
+  publicKey: publicKey.toString(),
+  address: address.toString()
+}
+
+fs.writeFile("key.json", JSON.stringify(key), "utf8", () => {
   console.log("wrote keyfile")
 })
