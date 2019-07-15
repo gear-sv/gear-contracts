dffa22f9e99bf4f23e47e567245d5003de7c3c0a
Sean Pollock <seanpollock3344@gmail.com>
2019-07-15 11:11:43 -0400

    ___'

--- scripts/createAccount.js
+++ scripts/createAccount.js
@@ -2,13 +2,10 @@ const bsv = require("bsv")
 const fs = require("fs")
 
 const privateKey = bsv.PrivateKey.fromRandom()
-console.log("private Key", privateKey.toString())
 
 const publicKey = bsv.PublicKey.fromPrivateKey(privateKey)
-console.log("publicKey", publicKey)
 
 const address = bsv.Address.fromPublicKey(publicKey).toString()
-console.log("address", address)
 
 const key = {
   privateKey: privateKey.toString(),
