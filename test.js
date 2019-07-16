b43baf2af0c939cffaf7e65f3b38d2a8d0b78432
Sean Pollock <seanpollock3344@gmail.com>
2019-07-15 20:57:13 -0400

    token.wasm

--- test.js
+++ test.js
@@ -1,31 +1,49 @@
+const test = require("tape")
+const bsv = require("bsv")
+const { promises: fs } = require("fs")
 const contractModule = require("./a.out.js")
 
 contractModule.onRuntimeInitialized = () => {
+  test("Token contract tests", async (t) => {
+    t.plan(9)
 
-  const address = "public_address"
+    const creator = "1EUSzmPjnDyvQgYTRukPDWaaeXSCHbCism"
 
-  console.log("setting owner of contract")
-  const setOwner = contractModule.cwrap("setOwner", "bool", ["string", "string"])
-  const setOwnerResult = setOwner(address, "sean")
+    const getOwner = contractModule.cwrap("getOwner", "string")
+    const owner = getOwner()
+    t.equal(creator, owner, "contract has been initialized with the creator as owner")
 
-  console.log("getting owner of the contract")
-  const getOwner = contractModule.cwrap("getOwner", "string")
-  const owner = getOwner()
-  console.log("owner is", owner)
+    const mint = contractModule.cwrap("mint", "bool", ["string", "number"])
+    const mintResult = mint(creator, 1000)
+    t.equal(mintResult, +true, "owner address able to mint tokens")
 
-  console.log("minting a 1000 tokens")
-  const mint = contractModule.cwrap("mint", "bool", ["string", "number"])
-  const mintResult = mint(address, 1000)
-  console.log("mintResult is", mintResult)
+    const getBalance = contractModule.cwrap("getBalance", "number", ["string"])
+    const balance = getBalance(creator)
+    t.equal(balance, 1000, "owner balance accepted minting amount of 1000 tokens")
 
-  console.log("checking total supply")
-  const getSupply = contractModule.cwrap("getSupply", "number")
-  const supply = getSupply()
-  console.log("total supply is", supply)
+    const getSupply = contractModule.cwrap("getSupply", "number")
+    const supply = getSupply()
+    t.equal(supply, 1000, "mint successfully increments supply by 1000 tokens")
 
-  console.log("calling transfer method")
-  const transfer = contractModule.cwrap("transfer", "bool", ["string", "string", "number"])
-  const transferResult = transfer(address, "glenn", 100)
+    const transfer = contractModule.cwrap("transfer", "bool", ["string", "string", "number"])
+    const transferResult = transfer(creator, "glenn", 100)
+    console.log("transferResult", transferResult)
+    t.equal(transferResult, +true, "succesfully transfered tokens")
+
+    const balance1 = getBalance(creator)
+    t.assert(balance1, 900, "succesfully decremented balance of sender by 100 tokens")
+
+    const balance2 = getBalance("glenn")
+    t.assert(balance2, 100, "succesfully incremented balance of glenn by 100 tokens")
+
+    const setOwner = contractModule.cwrap("setOwner", "bool", ["string", "string"])
+    const setOwnerResult = setOwner(creator, "sean")
+    console.log("setOwnerResult", setOwnerResult)
+    t.assert(setOwnerResult, +true, "succesfully changed owner")
+
+    const newOwner = getOwner()
+    console.log("newOwner", newOwner)
+    t.assert(newOwner, "sean", "new owner is set to sean")
+  })
 }
-
 
