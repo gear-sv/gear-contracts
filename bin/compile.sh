913ae0912cbe58c35b8d2fe190b907b505d5139b
Sean Pollock <seanpollock3344@gmail.com>
2019-08-01 01:13:03 -0400

    small fixes to compilation output

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,7 +2,5 @@ emcc $contract_path \
   -O0 -s \
   EXPORTED_FUNCTIONS=$functions -s \
   EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
-  -std=c++11
-
-mv a.out.wasm output/$wasm_file
-mv a.out.js output/$js_file
+  -std=c++11 \
+  -o $contract_name.out.js
