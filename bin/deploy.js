85a35bafab16b61de4cec04bfce05f55f626e188
Sean Pollock <seanpollock3344@gmail.com>
2019-09-17 23:59:06 -0400

    1. adding constructor to deploy payload 2. inquirer prompts for each constructor input variable

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,16 +1,30 @@
-const fs = require("fs")
+const inquirer = require("inquirer")
+const util = require("util")
+const readFile = util.promisify(require("fs").readFile)
 const datapay = require("datapay")
+
 const { readTar, readKey } = require("./gear-utils.js")
 
 const main = async (contract) => {
   // fetch key
   const key = await readKey()
 
+  // fetch contract package
   const blueprint = await readTar(contract)
+
+  // fetch constructor
+  const abi = await readFile(`${process.cwd()}/contracts/${contract}.json`)
+  const { constructor } = JSON.parse(abi)
+
+  const prompts = Object.keys(constructor).map(input => ({
+    type: "input", name: input, message: input, default: ""
+  }))
+
+  const constructorInputs = await inquirer.prompt(prompts)
 
   // format and send transaction
   datapay.send({
-    data: ["gear", blueprint],
+    data: ["gear", blueprint, ...Object.values(constructorInputs)],
     pay: {
       key: key.privateKey,
       fee: 0
